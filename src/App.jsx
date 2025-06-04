8a30fdd51ac94b181b7f5b2c57ed4270e803a098
Pierre Gronnier <pierre.gronnier@gmail.com>
2025-06-04 16:33:39 +0200

    init project and add background

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="background" />
+    </div>
+  );
+}
+
+export default App;
